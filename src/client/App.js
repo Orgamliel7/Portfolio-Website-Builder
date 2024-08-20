234d476fd4ec239d323f30e1781be2f9fb35c125
Orgamliel7 <orgamliel777@gmail.com>
2024-08-20 16:57:43 +0300

    App.js basic file

---
+++ src/client/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to Personal Portfolio Builder!</h1>
+      <p>Let's create something amazing together.</p>
+    </div>
+  );
+};
+
+export default App;
