d17c2d4b3bb6b954d2a0cef5342b9b2d840629f4
Orgamliel7 <orgamliel777@gmail.com>
2024-08-21 11:18:18 +0300

    content to header.js

---
+++ src/client/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const HeaderContainer = styled.header`
+  background-color: #f8f8f8;
+  padding: 20px;
+  text-align: center;
+`;
+
+const Title = styled.h1`
+  color: #333;
+  font-size: 2em;
+`;
+
+const Header = () => (
+  <HeaderContainer>
+    <Title>Personal Portfolio Builder</Title>
+  </HeaderContainer>
+);
+
+export default Header;
