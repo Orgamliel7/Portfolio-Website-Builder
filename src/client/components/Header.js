1801ca55c19c502e095f6965e5eafdecaedeb96c
or-mv <or.gamliel@mantis-vision.com>
2024-08-25 14:22:09 +0300

    Update header.js

--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -2,21 +2,43 @@ import React from 'react';
 import styled from 'styled-components';
 
 const HeaderContainer = styled.header`
-  background-color: #f8f8f8;
+  background-color: #3498db;
   padding: 20px;
   text-align: center;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 `;
 
 const Title = styled.h1`
-  color: #333;
+  color: white;
   font-size: 2em;
+  margin: 0;
+`;
+
+const NavMenu = styled.nav`
+  margin-top: 10px;
+`;
+
+const NavLink = styled.a`
+  color: white;
+  text-decoration: none;
+  margin: 0 10px;
+  font-size: 1.1em;
+  transition: opacity 0.3s ease;
+
+  &:hover {
+    opacity: 0.8;
+  }
 `;
 
 const Header = () => (
   <HeaderContainer>
     <Title>Personal Portfolio Builder</Title>
+    <NavMenu>
+      <NavLink href="#about">About</NavLink>
+      <NavLink href="#portfolio">Portfolio</NavLink>
+      <NavLink href="#contact">Contact</NavLink>
+    </NavMenu>
   </HeaderContainer>
 );
-
 
 export default Header;
